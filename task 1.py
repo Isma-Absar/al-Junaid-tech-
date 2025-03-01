5230606c3bc346b066153acc38a5abeff78ca32b
Isma-Absar <nomimohra@gmail.com>
2025-03-01 10:41:42 -0800

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,45 @@
+#declare two variable
+a=5
+b=4
+#perform operation
+addition=a+b
+subtraction=a-b
+division=a/b
+modulo=a%b
+#print result
+print(addition)
+print(subtraction)
+print(division)
+print(modulo)
+# declare two numerical variable 
+a=10
+b=5
+#compare variale using comparison ooperator
+equal=a==b
+not_equal=a!=b
+greator_than= a>b
+less_than=a<b
+#print result 
+#print (comparison result)
+print(equal)
+print(not_equal)
+print(greator_than)
+print(less_than)
+#declare varriale
+a=15
+#assignment operator
+a+=5
+a-=5
+a*=2
+a/=5
+print(a)
+#define boolean variable
+a=True
+b=False
+#logical conition
+and_result=a and b
+or_result= a or  b
+not_result=not a
+print(and_result)
+print(or_result)
+print(not_result)
